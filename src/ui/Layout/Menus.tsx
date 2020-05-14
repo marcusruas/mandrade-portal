ff65b2761d1cfd1f313a7a72902310ec92e44764
marcusruas <marcus.ruas@hotmail.com>
2020-05-13 22:03:47 -0300

    Início dos menus.

--- src/ui/Layout/Menus.tsx
+++ src/ui/Layout/Menus.tsx
@@ -5,14 +5,14 @@ const Menus = () => {
     const { Sider } = Layout;
     const { SubMenu } = Menu;
     return (
-        <Sider width={200}>
+        <Sider width={300}>
             <Menu className="Menus" mode="inline" style={{ height: '100%', borderRight: 0 }}>
-                <SubMenu key="usuarioApi" title="UsuarioAPI">
-                    <Menu.Item key="1">Grupos</Menu.Item>
-                    <Menu.Item key="2">Pessoas</Menu.Item>
-                    <Menu.Item key="3">Usuarios</Menu.Item>
-                    <Menu.Item key="4">Permissoes</Menu.Item>
-                    <Menu.Item key="5">Autenticação</Menu.Item>
+                <SubMenu key="usuario" title="UsuarioAPI">
+                    <SubMenu key="usuarioGrupos" title="Grupos">
+                        <Menu.Item key="1">Cadastrar novo grupo</Menu.Item>
+                        <Menu.Item key="2">Buscar grupo</Menu.Item>
+                        <Menu.Item key="3">Deletar grupo</Menu.Item>
+                    </SubMenu>
                 </SubMenu>
             </Menu>
         </Sider>
