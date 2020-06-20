a1fef90a966e89980bf1b2c03f5a32d6eec851fa
marcusruas <marcus.ruas@hotmail.com>
2020-06-19 21:36:07 -0300

    Correção de layout.

--- src/ui/SharedComponents/CabecalhoPagina.tsx
+++ src/ui/SharedComponents/CabecalhoPagina.tsx
@@ -1,27 +1,14 @@
 import React, { CSSProperties } from 'react';
+import './index.css';
 
 interface ParametrosCabecalho {
     Titulo: string
 }
 
 const CabecalhoPagina = (parametros: ParametrosCabecalho) => {
-    const estiloCabecalho: CSSProperties = {
-        width: '100%',
-        height: '10%',
-        backgroundColor: 'darkgray',
-    }
-
-    const estiloTexto: CSSProperties = {
-        padding: '12px',
-        textAlign: 'left',
-        color: '#030852',
-        fontWeight: 'bold',
-        fontSize: '20px'
-    }
-
     return (
-        <div style={estiloCabecalho}>
-            <h1 style={estiloTexto}>{parametros.Titulo}</h1>
+        <div className="CabecalhoPagina">
+            <h1 className="CabecalhoPagina_Titulo">{parametros.Titulo.toUpperCase()}</h1>
         </div>
     )
 }
